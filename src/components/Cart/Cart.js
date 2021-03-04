504dfd698499ee436c288ea2681e89317be173a2
MHP0720 <dmmehediparash@gmail.com>
2021-03-05 02:44:31 +0700

    almost complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
     // const totalPrice = cart.reduce((total, prd) =>total + prd.price , 0)
@@ -7,21 +8,21 @@ const Cart = (props) => {
     let totalPrice = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        totalPrice = totalPrice + product.price
+        totalPrice = totalPrice + product.price * product.quantity;
     }
 
-    let shipping = 12.99;
-    if(totalPrice > 35){
+    let shipping = 0;
+    if (totalPrice > 35) {
         shipping = 0;
     }
-    else if (totalPrice > 15){
+    else if (totalPrice > 15) {
         shipping = 4.99;
     }
-    else if(totalPrice > 0){
+    else if (totalPrice > 0) {
         shipping = 12.99;
     }
 
-    const tax =(totalPrice / 10).toFixed(2);
+    const tax = (totalPrice / 10).toFixed(2);
     const grandTotal = (totalPrice + shipping + Number(tax)).toFixed(2);
 
     const formatNumber = num => {
@@ -31,13 +32,16 @@ const Cart = (props) => {
 
     return (
         <div>
-            <h4>Order Summery</h4>
+            <h4 className="text-danger">Order Summery</h4>
             <p>Items Ordered: {cart.length}</p>
             <p>Product Price:{formatNumber(totalPrice)}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT:{tax}</small></p>
             <p>Total price:{grandTotal}</p>
-
+            <br />
+            {
+                props.children
+            }
         </div>
     );
 };
